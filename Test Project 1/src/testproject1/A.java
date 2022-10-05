c8c7f3ba28db352103b9efadfb16d94e524c5118
Shubham <Shubham@LAPTOP-3JLM5LLC>
2022-10-05 12:18:04 +0530

    A class is commited

---
+++ Test Project 1/src/testproject1/A.java
@@ -0,0 +1,15 @@
+package testproject1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		int i=109;
+		System.out.println(i);
+		System.out.println("45");
+		System.out.println(" +");
+		
+	}
+
+}
